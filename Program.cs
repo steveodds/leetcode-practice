44e554604ba3e5255c03f21788a14a0457bb4ca5
Stephen Odundo <steveremdi@gmail.com>
2020-02-02 21:29:22 +0300

    Solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace leetcode_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var indices = TwoSum(new int[] { 3, 2, 4 }, 6);
+            System.Console.WriteLine($"Indices: {indices[0]}, {indices[1]}");
+            Console.In.Read();
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            // var result = new int[2];
+            // var sum = 0;
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     for (int j = 0; j < nums.Length - 1; j++)
+            //     {
+            //         sum = nums[i] + nums[j + 1];
+            //         if (sum == target)
+            //         {
+            //             result[0] = i;
+            //             result[1] = j;
+            //             return result;
+            //         }
+            //     }
+            // }
+            // return result;
+
+            var result = new int[2];
+            foreach (var number in nums)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (number + nums[i] == target && Array.IndexOf(nums, number) != i)
+                    {
+                        result[1] = Array.IndexOf(nums, number);
+                        result[0] = i;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
