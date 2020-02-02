d89a38e4fc8b0b8de2e771d5e3d7e8940433273a
Stephen Odundo <steveremdi@gmail.com>
2020-02-02 20:55:56 +0300

    Created project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
